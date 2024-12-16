b68eac2ac49b871e3e4a2fe8f7c5a6b4f6a44ea9
nierdna <cassie382.618@gmail.com>
2024-12-16 15:28:03 +0700

    update

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,5 +1,15 @@
 import { registerAs } from "@nestjs/config";
 
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: process.env.DB_SYNC,
+});
+
+
 export default registerAs("database", () => ({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
